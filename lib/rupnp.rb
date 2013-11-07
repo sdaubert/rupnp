88a890baf24c1dcf232fa6f7d075bf44347321e3
Sylvain Daubert <sylvain.daubert@laposte.net>
2013-11-07 18:51:47 +0100

    * lib/rupnp.rb:   Add RUPNP.log_level= and RUPNP.log_level.

--- lib/rupnp.rb
+++ lib/rupnp.rb
@@ -5,6 +5,7 @@ module RUPNP
   VERSION = '0.1.0'
 
   @logdev = STDERR
+  @log_level = :info
 
   def self.logdev=(io_or_string)
     if io_or_string.is_a? String
@@ -18,10 +19,19 @@ module RUPNP
     @logdev
   end
 
+  def self.log_level=(lvl)
+    @log_level = lvl
+  end
+
+  def self.log_level
+    @log_level
+  end
+
 end
 
 
 require_relative 'rupnp/constants'
+require_relative 'rupnp/log_mixin'
 require_relative 'rupnp/control_point'
 require_relative 'rupnp/device'
 require_relative 'rupnp/ssdp'
