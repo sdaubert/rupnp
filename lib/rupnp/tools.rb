c4e4fc6c70c41201dc064fd41e9f81dfa01f716f
Sylvain Daubert <sylvain.daubert@laposte.net>
2013-12-08 15:45:11 +0100

    * lib/rupnp/tools.rb: add Tools#urn_are_equivalent? to compare   two urns.

--- lib/rupnp/tools.rb
+++ lib/rupnp/tools.rb
@@ -23,6 +23,37 @@ module RUPNP
       g.downcase || str.downcase
     end
 
+    # Check if two URN are equivalent. They are equivalent if they have
+    # the same name and the same major version.
+    # @param [String] urn1
+    # @param [String] urn2
+    # @return [Boolean]
+    def urn_are_equivalent?(urn1, urn2)
+      u1 = urn1
+      if urn1[0..3] == 'urn:'
+        u1 = urn1[4..-1]
+      end
+      u2 = urn2
+      if urn2[0..3] == 'urn:'
+        u2 = urn2[4..-1]
+      end
+
+      m1 = u1.match(/(\w+):(\w+):(\w+):([\d-]+)/)
+      m2 = u2.match(/(\w+):(\w+):(\w+):([\d-]+)/)
+      if m1[1] == m2[1]
+        if m1[2] == m2[2]
+          if m1[3] == m2[3]
+            v1_major = m1[4].split(/-/).first
+            v2_major = m2[4].split(/-/).first
+            if v1_major == v2_major
+              return true
+            end
+          end
+        end
+      end
+      false
+    end
+
   end
 
 end
