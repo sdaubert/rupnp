1e43d62eddc543d119a11e671aecec5d37db3a26
Sylvain Daubert <sylvain.daubert@laposte.net>
2014-01-07 20:14:02 +0100

    Write 'RUPNP::SSDP::Listener should ignore M-SEARCH requests' spec.

--- spec/ssdp/listener_spec.rb
+++ spec/ssdp/listener_spec.rb
@@ -7,13 +7,40 @@ module RUPNP
       include EM::SpecHelper
 
       it "should receive alive and byebye notifications"
-      it "should ignore M-SEARCH requests"
+
+      it "should ignore M-SEARCH requests" do
+        rd_io, wr_io = IO.pipe
+        begin
+          RUPNP.logdev = wr_io
+          RUPNP.log_level = :warn
+          em do
+            listener = SSDP.listen
+            listener.notifications.subscribe do |notification|
+              fail
+            end
+
+            searcher = SSDP.search(:all, :try_number => 1)
+
+            EM.add_timer(1) do
+              begin
+                warn = rd_io.read_nonblock(127)
+                expect(warn).to be_empty
+              rescue IO::WaitReadable
+              end
+              done
+            end
+          end
+        ensure
+         rd_io.close
+         wr_io.close
+        end
+      end
 
       it "should ignore and log unknown requests" do
         rd_io, wr_io = IO.pipe
-        RUPNP.logdev = wr_io
-        RUPNP.log_level = :warn
         begin
+          RUPNP.logdev = wr_io
+          RUPNP.log_level = :warn
           em do
             listener = SSDP.listen
             listener.notifications.subscribe do |notification|
