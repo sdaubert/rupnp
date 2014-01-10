5043aefcbcb5eb1e90c6dab6db8922906a88e635
Sylvain Daubert <sylvain.daubert@laposte.net>
2014-01-10 19:58:34 +0100

    * spec/spec_helper.rb: add filter on spec directory.
    * spec/ssdp/searcher_spec.rb: "should discard and log bad responses" spec pass

--- spec/ssdp/searcher_spec.rb
+++ spec/ssdp/searcher_spec.rb
@@ -47,8 +47,37 @@ module RUPNP
         end
       end
 
-      it "should discard and log bad responses"
+      it "should discard and log bad responses" do
+        reader, writer = IO.pipe
+        begin
+          RUPNP.logdev = writer
+          em do
+            receiver = EM.open_datagram_socket(MULTICAST_IP, DISCOVERY_PORT,
+                                               FakeMulticast)
+            receiver.onmessage do
+              receiver.send_data "HTTP/1.1 404 Not Found\r\n\r\n"
+              receiver.send_data "this is not an HTTP message!"
+            end
+            searcher = RUPNP::SSDP.search(:all, config.merge(:try_number => 1))
+            searcher.discovery_responses.subscribe do |n|
+              next unless n['server'] =~ /RUPNP/
+              fail
+            end
 
+            EM.add_timer(1) do
+              expect(reader.readline).to match(/\[error\] bad HTTP response/)
+              expect(reader.readline).to match(/404/)
+              reader.readline
+              expect(reader.readline).to match(/\[error\] bad HTTP response/)
+              expect(reader.readline).to match(/not/)
+              done
+            end
+          end
+        ensure
+          reader.close
+          writer.close
+        end
+      end
     end
 
   end
