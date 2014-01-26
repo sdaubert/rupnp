a8fb0e10223fe3e52315afe1ba826799874ad269
Sylvain Daubert <sylvain.daubert@laposte.net>
2014-01-26 15:00:58 +0100

    Make 'CP::RemoteService#fetch should fetch SCPD' spec pass.

--- spec/cp/remote_service_spec.rb
+++ spec/cp/remote_service_spec.rb
@@ -4,10 +4,32 @@ module RUPNP
 
     describe RemoteService do
       include EM::SpecHelper
+      include RUPNP::Tools
+
+      let(:url_base) { 'http://127.0.0.1:1234/' }
+      let(:sd) { {
+          :service_type => 'urn:schemas-upnp-org:service:Service:1-0',
+          :service_id => 'urn:upnp-org:serviceId:Service',
+          :scpdurl => '/service/description.xml',
+          :control_url => '/service/control',
+          :event_url => '/service/event' } }
+      let(:rs){ RemoteService.new(double('rdevice'), url_base, sd)}
 
       context '#fetch' do
 
-        it 'should fetch SCPD'
+        it 'should fetch SCPD' do
+          stub_request(:get, build_url(url_base, sd[:scpdurl])).
+            to_return(:headers => { 'SERVER' => 'OS/1.0 UPnP/1.1 TEST/1.0'},
+                      :body => generate_scpd)
+          em do
+            rs.errback { fail 'RemoteDevice#fetch should work' }
+            rs.callback do
+              done
+            end
+            rs.fetch
+          end
+        end
+
         it 'should set state table'
         it 'should define actions as methods from description'
         it 'should fail when no SCPD URL is given'
