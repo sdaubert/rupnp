63da5200793cbd93ab2ec68ddafe276a4692e559
Sylvain Daubert <sylvain.daubert@laposte.net>
2014-03-01 11:34:13 +0100

    Make 'RUPNP::Event should send timeout message' spec pass.

--- lib/rupnp/event.rb
+++ lib/rupnp/event.rb
@@ -14,13 +14,24 @@ module RUPNP
     # @param [String] callback_url Callback URL to receive events
     # @param [#to_i] sid
     # @param [Integer] timeout for event (in seconds)
-    def initialize(event_suburl, callback_url, sid, timeout)
+    # @param [Hash] options
+    # @option options [Boolean] :auto_renew If true, automatically renew
+    #   subscription before timeout. If false, a message +:timeout+ is
+    #   sent throught +self+. (default: +true+)
+    # @option options [Integer] :before_timeout number of seconds before
+    #   timeout to renew subscription or send +:timeout+ message.
+    #   (default: 60)
+    def initialize(event_suburl, callback_url, sid, timeout, options={})
       super()
       @event_suburl = event_suburl
       @callback_url = callback_url
       @sid, @timeout = sid, timeout
+      @options = {
+        :auto_renew => true,
+        :before_timeout => 60,
+      }.merge(options)
 
-      @timeout_timer = EM.add_timer(@timeout) { self << :timeout }
+      set_timer
     end
 
     # Renew subscription to event
@@ -33,6 +44,20 @@ module RUPNP
     # @todo
     def cancel_subscription
       raise NotImplementedError
+    end
+
+
+    private
+
+    def set_timer
+      if @options[:auto_renew]
+        blk = Proc.new { renew_subscription }
+      else
+        blk =  Proc.new { self << :timeout }
+      end
+
+      time = @timeout - @options[:before_timeout].to_i
+      @timeout_timer = EM.add_timer(time, &blk)
     end
 
   end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -3,8 +3,19 @@ require_relative 'spec_helper'
 module RUPNP
 
   describe Event do
+    include EM::SpecHelper
 
-    it 'should send timeout message'
+    it 'should send timeout message' do
+      em do
+        ev = Event.new('', '', '', 1, :auto_renew => false,
+                       :before_timeout => 0)
+        ev.subscribe do |msg|
+          expect(msg).to eq(:timeout)
+          done
+        end
+      end
+    end
+
     it 'should renew subscription on demand'
     it 'should automatically renew subscription'
     it 'should cancel subscription'
