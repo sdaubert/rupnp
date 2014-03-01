94f922dd33865fd5e2a1fb215e5d72cd238b5123
Sylvain Daubert <sylvain.daubert@laposte.net>
2014-03-01 16:06:28 +0100

    Event#renew_subscription added.

--- lib/rupnp/event.rb
+++ lib/rupnp/event.rb
@@ -1,18 +1,37 @@
 module RUPNP
 
   # Event class to handle events from devices
-  # @todo Renewal and cancellation of subscription are not coded
+  # @todo Cancellation of subscription are not coded
+  #
+  # ==Messages
+  # {Event} class receives messages. As {Event} is a subclass of
+  # EM::Channel, user may subscribe to it to receive these messages:
+  #   event.subscribe do |msg|
+  #     # use msg
+  #   end
+  # Possible messages are:
+  # * +:timeout+, receive +:before_timeout+ seconds before real time out,
+  # * +:renew+ to say to user that automatic renewing occured,
+  # * +:cancelled+ to say to user that subscription was cancelled.
+  #   Cancellation may occur on demand (see {#cancel_subscription}), or
+  #   on error when renewing failed.
   # @author Sylvain Daubert
   class Event < EM::Channel
+    include LogMixin
 
     # Get service ID
     # @return [Integer]
     attr_reader :sid
+    # Get callback URL
+    # @return [String]
     attr_reader :callback_url
+    # Get timeout in seconds
+    # @return [Integer]
+    attr_reader :timeout
 
     # @param [String] event_suburl Event subscription URL
     # @param [String] callback_url Callback URL to receive events
-    # @param [#to_i] sid
+    # @param [#to_s] sid
     # @param [Integer] timeout for event (in seconds)
     # @param [Hash] options
     # @option options [Boolean] :auto_renew If true, automatically renew
@@ -23,7 +42,7 @@ module RUPNP
     #   (default: 60)
     def initialize(event_suburl, callback_url, sid, timeout, options={})
       super()
-      @event_suburl = event_suburl
+      @event_sub_url = event_suburl
       @callback_url = callback_url
       @sid, @timeout = sid, timeout
       @options = {
@@ -35,9 +54,34 @@ module RUPNP
     end
 
     # Renew subscription to event
-    # @todo
     def renew_subscription
-      raise NotImplementedError
+      uri = URI(@event_sub_url)
+      con = EM::HttpRequest.new(@event_sub_url)
+      log :debug, 'Open connection for renewing subscription'
+      http = con.setup_request(:subscribe, :head => {
+                                 'HOST' => "#{uri.host}:#{uri.port}",
+                                 'USER-AGENT' => RUPNP::USER_AGENT,
+                                 'SID' => @sid,
+                                 'TIMEOUT' => "Second-#@timeout"})
+      http.errback do |client|
+        log :warn, "Cannot renew subscription to event: #{client.error}"
+        con.close
+      end
+
+      http.callback do
+        log :debug, 'Close connection'
+        con.close
+        if http.response_header.status != 200
+          log :warn, "Cannot renew subscribtion to event #@event_sub_url:" +
+            " #{http.response_header.http_reason}"
+        else
+          @timeout = http.response_header['TIMEOUT'].match(/(\d+)/)[1].to_i ||
+            1800
+          set_timer
+          log :info, "Subscription to #@event_sub_url renewed"
+          self << :renewed
+        end
+      end
     end
 
     # Cancel subscription to event
@@ -50,6 +94,8 @@ module RUPNP
     private
 
     def set_timer
+      EM.cancel_timer(@timeout_timer) if @timeout_timer
+
       if @options[:auto_renew]
         blk = Proc.new { renew_subscription }
       else
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -5,6 +5,13 @@ module RUPNP
   describe Event do
     include EM::SpecHelper
 
+    let(:host) { '192.169.15.48:45678' }
+    let(:location) { "#{host}/event_subscription" }
+    let(:ev_suburl) { "http://#{location}" }
+    let(:sid) { "uuid:#{UUID.generate}" }
+    let(:options) { { :auto_renew => true, :before_timeout => 1 } }
+    let(:ev) { Event.new(ev_suburl, '', sid, 2, options)}
+
     it 'should send timeout message' do
       em do
         ev = Event.new('', '', '', 1, :auto_renew => false,
@@ -16,8 +23,46 @@ module RUPNP
       end
     end
 
-    it 'should renew subscription on demand'
-    it 'should automatically renew subscription'
+    it 'should renew subscription on demand' do
+      em do
+        stub_request(:subscribe, location).
+          with(:headers => { 'HOST' => host, 'SID' => sid,
+               'TIMEOUT' => "Second-#{ev.timeout}"}).
+          to_return(:status => 200,
+                    :headers => {
+                      'SERVER' => 'OS/1.0 UPnP/1.1 TEST/1.0',
+                      'SID' => sid,
+                      'TIMEOUT' => 'Second-1800'
+                    })
+        ev.renew_subscription
+        ev.subscribe do |msg|
+          expect(msg).to eq(:renewed)
+          expect(ev.timeout).to eq(1800)
+          done
+        end
+      end
+    end
+
+    it 'should automatically renew subscription' do
+      em do
+        stub_request(:subscribe, location).
+          with(:headers => { 'HOST' => host, 'SID' => sid,
+               'TIMEOUT' => "Second-#{ev.timeout}"}).
+          to_return(:status => 200,
+                    :headers => {
+                      'SERVER' => 'OS/1.0 UPnP/1.1 TEST/1.0',
+                      'SID' => sid,
+                      'TIMEOUT' => 'Second-1800'
+                    })
+
+        ev.subscribe do |msg|
+          expect(msg).to eq(:renewed)
+          expect(ev.timeout).to eq(1800)
+          done
+        end
+      end
+    end
+
     it 'should cancel subscription'
   end
 
