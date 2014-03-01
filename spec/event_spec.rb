4fde4261e51c19f69fcadaa2ce1a13c56200aee6
Sylvain Daubert <sylvain.daubert@laposte.net>
2014-03-01 11:30:21 +0100

    Add specs for Event.

---
+++ spec/event_spec.rb
@@ -0,0 +1,13 @@
+require_relative 'spec_helper'
+
+module RUPNP
+
+  describe Event do
+
+    it 'should send timeout message'
+    it 'should renew subscription on demand'
+    it 'should automatically renew subscription'
+    it 'should cancel subscription'
+  end
+
+end
